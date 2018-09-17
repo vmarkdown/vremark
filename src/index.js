ebd157fe46139cf099ea61eb9dc030c17c569a61
yucopowo <yucopowo@gmail.com>
2018-09-17 23:55:39 +0800

    init

--- src/index.js
+++ src/index.js
@@ -3,25 +3,41 @@ const parse = require('remark-parse');
 const toc = require('remark-toc');
 const math = require('remark-math');
 const linkReference = require('./plugins/linkReference/index');
-const paragraphHTML = require('./plugins/paragraph-html/index');
+const paragraphHTML2Text = require('./plugins/paragraph-html-to-text/index');
 const checkbox = require('./plugins/checkbox/index');
-const vdom = require('./plugins/vdom/index');
+// const render = require('remark-render');
 
-function vremark(options) {
-    return unified()
-        .use(parse, options)
+// const Renderer = require('remark-render/renderers/react-renderer');
+
+module.exports = unified()
+        .use(parse, {})
         .use(toc, {
             // heading: '[TOC]'
         })
-        .use(linkReference, options)
-        .use(paragraphHTML, options)
-        .use(checkbox, options)
-        .use(math, options)
-        .use(vdom, options);
-}
+        .use(linkReference, {})
+        .use(paragraphHTML2Text, {})
+        .use(checkbox, {})
+        .use(math, {})
+        // .use(render, {})
+    .freeze();
 
-vremark.Renderer = require('./renderers/renderer');
-module.exports = vremark;
+// module.exports = unified().use(parse).use(stringify).freeze();
+
+// function vremark(options) {
+//     return unified()
+//         .use(parse, options)
+//         .use(toc, {
+//             // heading: '[TOC]'
+//         })
+//         .use(linkReference, options)
+//         .use(paragraphHTML, options)
+//         .use(checkbox, options)
+//         .use(math, options)
+//         .use(render, options);
+// }
+//
+// vremark.Renderer = require('./renderers/renderer');
+// module.exports = vremark;
 
 
 
